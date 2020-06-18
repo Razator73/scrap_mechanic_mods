0c869d53ad71cb57e8a36d9fa34fee3e6247ed98
Ryan Scott <rscott@bestcompany.com>
2020-06-18 11:11:06 -0600

    Ignore id if it already has a comment line above it.

--- add_craft_names.py
+++ add_craft_names.py
@@ -8,11 +8,13 @@ def add_item_names(file, names):
         recipe_lines = f.readlines()
 
     item_id_regex = re.compile(r'(\t*)"itemId": "([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})"')
+    comment_line = False
     commented_lines = []
     for line in recipe_lines:
-        if line_search := item_id_regex.search(line):
+        if (line_search := item_id_regex.search(line)) and not comment_line:
             tabs, item_id = line_search.groups()
             commented_lines.append(f'{tabs}// {names[item_id]}\n')
+        comment_line = line.strip().startswith('//')
         commented_lines.append(line)
 
     with open(file, 'w') as f:
