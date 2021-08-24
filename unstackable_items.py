b42229f6651eb6d264a244ef5d97ba54d9712c8f
Ryan Scott <rscott@bestcompany.com>
2021-08-24 10:15:36 -0600

    Code for easily adding stackSize=10 to items

--- unstackable_items.py
+++ unstackable_items.py
@@ -2,14 +2,26 @@ import json
 from pathlib import Path
 
 
-shape_sets_path = Path('game_files/Survival/Objects/Database/ShapeSets')
+shape_sets_path = Path('new_install/Survival/Objects/Database/ShapeSets')
+item_file = Path('item_names.json')
+with open(item_file) as f:
+    item_names = json.load(f)
 
-
+non_stackables = {}
 for file in shape_sets_path.glob('*.json'):
-    print(file)
     try:
         with open(file) as f:
-            data = json.load(f)
-        print(data.keys())
+            shape_data = json.load(f)
+        for item in shape_data.get('partList', []):
+            if item.get('stackSize') is None and item.get('uuid'):
+                item_name = item_names.get(item['uuid'], {'title': 'ITEM NOT FOUND'})['title']
+                non_stackables[item['uuid']] = {'name': item_name, 'file': file.name}
+                if input(f'Make {item_name} stackable? (y/n)').lower() == 'y':
+                    item['stackSize'] = 10
+        with open(file, 'w') as f:
+            json.dump(shape_data, f, indent='\t')
     except json.decoder.JSONDecodeError:
-        print('Probably includes comment lines')
+        print(f'{file.name} - Probably includes comment lines')
+
+for k, v in non_stackables.items():
+    print(f'{k}: {v}')
