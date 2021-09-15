68fa4b68104954aa445e1c8a957c5d449ce5ca71
Ryan Scott <rscott@bestcompany.com>
2021-09-15 11:02:00 -0600

    Fix size bug where it was being saved as string and not int

--- unstackable_items.py
+++ unstackable_items.py
@@ -18,9 +18,10 @@ for file in [_ for path in shape_sets_paths for _ in path.glob('*.json')]:
             if item.get('stackSize') is None and item.get('uuid'):
                 item_name = item_names.get(item['uuid'], {'title': 'ITEM NOT FOUND'})['title']
                 non_stackables[item['uuid']] = {'name': item_name, 'file': file.name}
-                if add_stack_size and (size := input(f'Make {item_name} stackable? (y/n)').lower())[0] != 'n':
+                size = input(f'Make {item_name} stackable? (y/n)')
+                if add_stack_size and size.lower()[0] != 'n':
                     try:
-                        int(size)
+                        size = int(size)
                         item['stackSize'] = size
                     except ValueError:
                         item['stackSize'] = 10
