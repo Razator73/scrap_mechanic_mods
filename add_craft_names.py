487f1fa5b13ad3a2e9e3e038f3253a0757cd1b80
Ryan Scott <rscott@bestcompany.com>
2022-09-15 10:58:36 -0600

    Fant mod changes

--- add_craft_names.py
+++ add_craft_names.py
@@ -8,6 +8,7 @@ def add_item_names(file, names):
         recipe_lines = f.readlines()
 
     item_id_regex = re.compile(r'(\t*)"itemId": "([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})"')
+    miss_ids = []
     # comment_line = False
     commented_lines = []
     for line in recipe_lines:
@@ -20,19 +21,22 @@ def add_item_names(file, names):
                 commented_lines.append(f'{tabs}// {item_name["title"]}\n')
             else:
                 print(f'No item found for {item_id}')
+                miss_ids.append(miss_ids)
         commented_lines.append(line)
 
     with open(file, 'w') as f:
         f.writelines(commented_lines)
 
+    return miss_ids
+
 
 if __name__ == '__main__':
-    craftbot_path = Path('new_install/Survival/CraftingRecipes/craftbot.json')
-    hideout_path = Path('new_install/Survival/CraftingRecipes/hideout.json')
-    item_names_file = Path('item_names.json')
+    craftbot_path = Path('fant_mod/Survival/CraftingRecipes/craftbot.json')
+    hideout_path = Path('fant_mod/Survival/CraftingRecipes/hideout.json')
+    item_names_file = Path('item_names_fant.json')
 
     with open(item_names_file) as i:
         item_names = json.load(i)
 
-    add_item_names(craftbot_path, item_names)
-    add_item_names(hideout_path, item_names)
+    missing_ids = add_item_names(craftbot_path, item_names)
+    missing_ids += add_item_names(hideout_path, item_names)
